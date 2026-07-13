2b305e90804972a05fc59ab73a7701ec90e3e34e
ofurkanmeral <fom1329@gmail.com>
2026-07-13 10:02:01 +0300

    redis auht cache

--- src/CleanArt.Infrastructure/Authorization/AuthorizationService.cs
+++ src/CleanArt.Infrastructure/Authorization/AuthorizationService.cs
@@ -1,4 +1,5 @@
-﻿using CleanArt.Domain.Users;
+﻿using CleanArt.Application.Abstractions.Cache;
+using CleanArt.Domain.Users;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,13 +12,24 @@ namespace CleanArt.Infrastructure.Authorization
     internal sealed partial class AuthorizationService
     {
         private readonly ApplicationDbContext _context;
+        private readonly ICacheService _cacheService;
 
-        public AuthorizationService(ApplicationDbContext context)
+        public AuthorizationService(ApplicationDbContext context, ICacheService cacheService)
         {
             _context = context;
+            _cacheService = cacheService;
         }
         public async Task<UserRolesResponse>GetRolesForUserAsync(string identityId)
         {
+            var cacheKey = $"auth:roles-{identityId}";
+
+            var cacheRoles = await _cacheService.GetAsync<UserRolesResponse>(cacheKey);
+
+            if(cacheRoles is not null)
+            {
+                return cacheRoles;
+            }
+
             var roles = await _context.Set<User>()
                 .Where(user => user.IdentityId == identityId)
                 .Select(user => new UserRolesResponse
@@ -26,17 +38,30 @@ namespace CleanArt.Infrastructure.Authorization
                     Roles = user.Roles.ToList()
                 }).FirstAsync();
 
+            await _cacheService.SetAsync(cacheKey, roles);
+
             return roles;
         }
 
         public async Task<HashSet<string>>GetPermissionForUserAsync(string identityId)
         {
+            var cacheKey = $"auth:permissions-{identityId}";
+
+            var cachePermissions=await _cacheService.GetAsync<HashSet<string>>(cacheKey);
+
+            if(cachePermissions is not null)
+            {
+                return cachePermissions;
+            }
+
             var permissions=await _context.Set<User>()
                 .Where(user=>user.IdentityId==identityId)
                 .SelectMany(user=>user.Roles.Select(role=>role.Permissions))
                 .FirstAsync();
 
             var permissionSet=permissions.Select(x=>x.Name).ToHashSet();
+
+            await _cacheService.SetAsync(cacheKey, permissionSet);
 
             return permissionSet;
         }
