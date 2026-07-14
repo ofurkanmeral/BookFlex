8aa37e68e1e81d6e91d08f135c2cf3f9438881b1
ofurkanmeral <fom1329@gmail.com>
2026-07-14 06:16:25 +0300

    cache implementation

---
+++ src/CleanArt.Application/Abstractions/Cache/ICachedQuery.cs
@@ -0,0 +1,18 @@
+﻿using CleanArt.Application.Abstractions.Messaging.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArt.Application.Abstractions.Cache
+{
+    public interface ICachedQuery<TResponse> : IQuery<TResponse>,ICachedQuery;
+    
+    public interface ICachedQuery
+    {
+        string CacheKey { get; }
+        TimeSpan? Expiration { get; }
+    }
+    
+}
--- src/CleanArt.Application/Booking/GetBooking/GetBookingQuery.cs
+++ src/CleanArt.Application/Booking/GetBooking/GetBookingQuery.cs
@@ -1,6 +1,12 @@
-﻿using CleanArt.Application.Abstractions.Messaging.Query;
+﻿using CleanArt.Application.Abstractions.Cache;
+using CleanArt.Application.Abstractions.Messaging.Query;
 
 namespace CleanArt.Application.Booking.GetBooking
 {
-    public sealed record GetBookingQuery(Guid BookingId) : IQuery<BookingResponse>;
+    public sealed record GetBookingQuery(Guid BookingId) : ICachedQuery<BookingResponse>
+    {
+        public string CacheKey => $"booking-{BookingId}";
+
+        public TimeSpan? Expiration => null;
+    }
 }
